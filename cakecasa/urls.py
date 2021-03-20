1294e28ca39a3a9c906277abca9919aef51ca0c7
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-03-20 01:08:14 +0100

    added the urls for contact us

--- cakecasa/urls.py
+++ cakecasa/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('casa.urls', namespace='shop')),
+    path('', include('casa.urls', namespace='casa')),
 ]
 
 if settings.DEBUG:
