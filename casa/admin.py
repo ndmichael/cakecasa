9843d17bbec1d8037220fb0b6c14d969e7afb816
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-03-24 00:37:11 +0100

    registered data models to admin

--- casa/admin.py
+++ casa/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import CakeCategory, CakeProduct
 
 # Register your models here.
+
+
+@admin.register(CakeCategory)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(CakeProduct)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['cake_name', 'slug', 'price','available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('cake_name',)}
