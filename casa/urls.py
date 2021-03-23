a9e0012a0e660e9f4d14c0e732a55ea053e01532
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-03-24 00:40:17 +0100

    url for item_list

--- casa/urls.py
+++ casa/urls.py
@@ -4,11 +4,13 @@ from casa import views as shop_views
 app_name = 'casa'
 
 urlpatterns = [
-    # path('category/', shop_views.product_list, name='product_list'),
-    # path('<slug:category_slug>/', shop_views.product_list, name='product_list_by_category'),
-    # path('<int:id>/<slug:slug>/', shop_views.product_detail, name='product_detail'),   
     path('', shop_views.index, name='casa-home'),
     path('about/', shop_views.about, name='casa-about'),
     path('contact_us/', shop_views.contact_view, name='casa-contact'),
+    path('cakes/', shop_views.contact_view, name='casa-contact'),
+    path('cakecategory/', shop_views.cake_list, name='cake_list'),
+    path('<slug:category_slug>/', shop_views.cake_list, name='cake_list_by_category'),
+    # path('<int:id>/<slug:slug>/', shop_views.product_detail, name='product_detail'),   
+    
 
 ]
