77041bd33890322c9aad057c8ef7e7efba3b1f22
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-03-30 01:01:26 +0100

    updated url for cake details

--- casa/urls.py
+++ casa/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('cakes/', shop_views.contact_view, name='casa-contact'),
     path('cakecategory/', shop_views.cake_list, name='cake_list'),
     path('<slug:category_slug>/', shop_views.cake_list, name='cake_list_by_category'),
+    path('<int:id>/<slug:slug>/', shop_views.cake_detail, name='cake_detail'),
     # path('<int:id>/<slug:slug>/', shop_views.product_detail, name='product_detail'),   
     
-
 ]
