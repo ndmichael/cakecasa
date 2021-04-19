29ab61e9a7f6fb3e68c13f90ac3b2482ce31709f
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-04-19 12:41:50 +0100

    updated settings added aws s3

--- cakecasa/settings.py
+++ cakecasa/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY_CAKECASA')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE') == 'True'
 
 ALLOWED_HOSTS = ['cakecasapasti.herokuapp.com', '127.0.0.1']
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -136,5 +137,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID_CAKECASA') 
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY_CAKECASA') 
+AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME_CAKECASA') 
+
+
+AWS_S3_FILE_OVERWRITE=False
+AWS_S3_REGION_NAME='us-east-2'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+AWS_DEFAULT_UCL=None
+
+if not DEBUG:
+    DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
